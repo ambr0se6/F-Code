840734a943f20fffe9e99783860861b0e1acc3d0
Ryan Burgett <thesonicdoctor@gmail.com>
2017-03-14 13:13:53 -0400

    Loaded Assignment 4 template

---
+++ assignment4.fs
@@ -0,0 +1,69 @@
+type id = string
+
+type term =
+  | Var of id
+  | Const of int 
+  | Term of id * term list
+
+(* invariant for substitutions: *)
+(* no id on a lhs occurs in any term earlier in the list *)
+type substitution = (id * term) list
+
+(* check if a variable occurs in a term *)
+let rec occurs (x : id) (t : term) : bool = failwith "Not implemented"
+
+(* substitute term s for all occurrences of variable x in term t *)
+let rec subst (s : term) (x : id) (t : term) : term = failwith "Not implemented"
+
+
+(* apply a substitution right to left; use foldBack *)
+let apply (s : substitution) (t : term) : term = failwith "Not implemented"
+
+
+(* unify one pair *)
+let rec unify (s : term) (t : term) : substitution = failwith "Not implemented"
+
+(* unify a list of pairs *)
+and unify_list (s : (term * term) list) : substitution = failwith "Not implemented"
+
+(*
+Examples
+> let t1 = Term("f",[Var "x";Var "y"; Term("h",[Var "x"])]);;
+val t1 : term = Term ("f",[Var "x"; Var "y"; Term ("h",[Var "x"])])
+> let t2 = Term("f", [Term("g",[Var "z"]); Term("h",[Var "x"]); Var "y"]);;
+val t2 : term =
+  Term ("f",[Term ("g",[Var "z"]); Term ("h",[Var "x"]); Var "y"])
+> let t3 = Term("f", [Var "x"; Var "y"; Term("g", [Var "u"])]);;
+val t3 : term = Term ("f",[Var "x"; Var "y"; Term ("g",[Var "u"])])
+> unify t1 t2;;
+val it : substitution =
+  [("x", Term ("g",[Var "z"])); ("y", Term ("h",[Var "x"]))]
+> let t4 = Term("f", [Var "x"; Term("h", [Var "z"]); Var "x"]);;
+val t4 : term = Term ("f",[Var "x"; Term ("h",[Var "z"]); Var "x"])
+>  let t5 = Term("f", [Term("k", [Var "y"]); Var "y"; Var "x"]);;
+val t5 : term = Term ("f",[Term ("k",[Var "y"]); Var "y"; Var "x"])
+> unify t4 t5;;
+val it : substitution =
+  [("x", Term ("k",[Term ("h",[Var "z"])])); ("y", Term ("h",[Var "z"]))]
+> unify t5 t4;;
+val it : substitution =
+  [("x", Term ("k",[Term ("h",[Var "z"])])); ("y", Term ("h",[Var "z"]))]
+> apply it t4;;
+val it : term =
+  Term
+    ("f",
+     [Term ("k",[Term ("h",[Var "z"])]); Term ("h",[Var "z"]);
+      Term ("k",[Term ("h",[Var "z"])])])
+> let t6 = Term("f", [Const 2; Var "x"; Const 3]);;
+val t6 : term = Term ("f",[Const 2; Var "x"; Const 3])
+> let t7 = Term("f", [Const 2; Const 3; Var "y"]);;
+val t7 : term = Term ("f",[Const 2; Const 3; Var "y"])
+> unify t6 t7;;
+val it : substitution = [("x", Const 3); ("y", Const 3)]
+> apply it t7;;
+val it : term = Term ("f",[Const 2; Const 3; Const 3])
+> unify t1 t7;;
+System.Exception: not unifiable: term constant clash
+....... junk removed .............
+Stopped due to error
+*)
